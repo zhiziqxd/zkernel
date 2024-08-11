b8b7c118cf5bd4c9844441ca0e6c4d9a28f7e0a1
zhiziqxd <166890319+zhiziqxd@users.noreply.github.com>
2024-08-11 22:32:34 +0800

    Update random.h

--- system/lib/random.h
+++ system/lib/random.h
@@ -1,12 +1,12 @@
 #ifndef random_H
 #define random_H
-const int RAND_MAX=
+const int RAND_MAX=32767;
 long long int rand_start;
 void srand(long long int st){
     rand_start=st;
 }
 int rand(){
     rand_start--;
-    return rand_start*2-4+1-4/3%1045+3435-56%32767;
+    return rand_start*2-4+1-4/3%1045+3435-56%RAND_MAX;
 }
 #endif
