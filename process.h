3d275a2bd8ff90a20f585c80ffcb5fac1d96cc45
zhiziqxd <166890319+zhiziqxd@users.noreply.github.com>
2024-08-16 22:37:11 +0800

    Update process.h

--- process.h
+++ process.h
@@ -1,4 +1,15 @@
-#ifndef file_H
-#define file_H
-#include <stdint.h>
+#ifndef procces_H
+#define procces_H
+#include "lib\io.h"
+#include "lib\system.h"
+void io_hlt(int long_){
+    asm("hlt");
+    asm("ret");
+    return;
+}
+void io_cli(){
+    asm("cli flag");
+    asm("sti");
+    return;
+}
 #endif
