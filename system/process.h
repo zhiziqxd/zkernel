34d0f0218691f4b7893ab6ac89e02ef8a07ec2ba
zhiziqxd <166890319+zhiziqxd@users.noreply.github.com>
2024-09-01 19:37:49 +0800

    Update process.h

--- system/process.h
+++ system/process.h
@@ -2,7 +2,31 @@
 #define procces_H
 #include "lib\io.h"
 #include "lib\system.h"
-void io_hlt(int long_){
+struct process{
+	int around;
+	void (*func)();
+	bool end_run=false;
+}pro[9999];
+int process_cnt;
+void add_process(int a,void (*b)()){
+	process_cnt++;
+	pro[process_cnt].around=a;
+	pro[process_cnt].func=b;
+	return;
+}
+void run_pro(){
+	int minn=1e9,in;
+	for(int i=1;i<=process_cnt;i++){
+		if(minn>pro[i].around&&!pro[i].end_run){
+			minn=pro[i].around;
+			in=i;
+		}
+	}
+	pro[in].func();
+	pro[in].end_run=true;
+	return;
+}
+void io_hlt(){
     asm("hlt");
     asm("ret");
     return;
